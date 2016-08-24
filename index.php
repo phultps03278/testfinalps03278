de346d76abeba1cc7a9a35b91cff99109be14c7a
phultps03278 <phultps03278@fpt.edu.vn>
2016-08-24 13:27:28 +0700

    commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=koi8-r" />
+<title>Website Template</title>
+<LINK HREF="style.css" rel="stylesheet" TYPE="text/css">
+<script src="active.js" type="text/javascript"></script>
+<script src="resize.js" type="text/javascript"></script>
+<script language="JavaScript" src="active.js" type="text/javascript"></script>
+</head>
+<body leftmargin="0" marginheight="0" marginwidth="0" rightmargin="0" topmargin="0" bgcolor="#1D6BFA">
+<center><table cellpadding="0" cellspacing="0" height="100%"  width="100%" border="0">
+	<tr>
+		<td height="100%" width="100%">
+	<script type="text/javascript">
+  AC_FL_RunContent('codebase','http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0','scale','noscale','width','100%','height','930','id','flash','align','top','src','flash','quality','high','bgcolor','#000000','name','flash','allowscriptaccess','sameDomain','pluginspage','http://www.macromedia.com/go/getflashplayer', 'movie','flash' );
+</script>
+<noscript>
+<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="100%" height="930" id="flash" align="middle">
+<param name="allowScriptAccess" value="sameDomain" />
+<param name="movie" value="flash.swf" />
+<param name="quality" value="high" />
+<param name="scale" value="noscale" />
+<param name="salign" value="t" />
+<param name="bgcolor" value="#000000" />
+<embed src="flash.swf" quality="high" scale="noscale" salign="t" bgcolor="#000000" width="100%" height="930" name="flash" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
+</object>
+</noscript>
+</td>
+	</tr>	
+</table></center>
+</body>
+</html>
